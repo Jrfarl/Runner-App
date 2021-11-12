22ca68d417044bc8f643b74bcc4e3c688dd14b36
Jrfarl <jrfarl0545@ung.edu>
2021-11-11 20:41:19 -0500

    Navigation Fully functional + minor aesthetic changes

--- app/src/main/java/com/zybooks/runnerapp/MainActivity.java
+++ app/src/main/java/com/zybooks/runnerapp/MainActivity.java
@@ -5,19 +5,15 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
@@ -27,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
             NavController navController = navHostFragment.getNavController();
 
             AppBarConfiguration appBarConfig = new AppBarConfiguration.Builder(
-                    R.id.navigation_home, R.id.navigation_options,
-                    R.id.navigation_scores, R.id.navigation_help).build();
+                    R.id.navigation_home, R.id.navigation_options, R.id.navigation_scores,
+                    R.id.navigation_help).build();
 
             NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
             NavigationUI.setupWithNavController(navView, navController);

3195d370b7baecea71a106fd0cdfabe6b31d49a6
Jrfarl <jrfarl0545@ung.edu>
2021-12-01 19:55:00 -0500

    Background Scroll

--- app/src/main/java/com/zybooks/runnerapp/MainActivity.java
+++ app/src/main/java/com/zybooks/runnerapp/MainActivity.java
@@ -5,15 +5,27 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.q42.android.scrollingimageview.ScrollingImageView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Forced full screen and landscape layout
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+
+        ScrollingImageView scrollingBackground = (ScrollingImageView) findViewById(R.id.scrolling_background);
+        scrollingBackground.start();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
